377f9dceb66e19d86e81bc1d9d66a6655ff3dfea
wito <tom_wilhelm@gmx.de>
2024-10-21 12:05:05 +0200

    initial commit

---
+++ src/main/java/de/efi23a/db_app_backend/DbAppBackendApplication.java
@@ -0,0 +1,13 @@
+package de.efi23a.db_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/efi23a/db_app_backend/DbAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.efi23a.db_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
