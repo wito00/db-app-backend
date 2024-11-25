e6d76d7f44871c3cf5ef6fdc0b19139f6d77f5e7
TO2398 <tom.wille@regiocom.com>
2024-11-25 12:44:19 +0100

    Added two new API´s. Now its possible to get departures of a specific station and if the station has an elevator.

--- src/main/java/de/efi23a/db_app_backend/api/controller/Controller.java
+++ src/main/java/de/efi23a/db_app_backend/api/controller/Controller.java
@@ -1,15 +1,17 @@
 package de.efi23a.db_app_backend.api.controller;
 
-//import de.efi23a.db_app_backend.service.DBService;
-//import org.example.apidemo.dbapi.TimetablesApi;
-//import org.example.apidemo.dbpapimodell.StationData;
-//import org.example.apidemo.dbpapimodell.Timetable;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.efi23a.db_app_backend.model.Departure;
 import de.efi23a.db_app_backend.model.DepartureList;
-import org.example.apidemo.dbapi.DefaultApi;
+import de.efi23a.db_app_backend.service.DBService;
+import org.example.dbREst.api.DefaultApi;
+import org.example.faSta.api.FaStaApi;
+import org.example.faSta.model.Facility;
+import org.example.timetables.api.TimetablesApi;
+import org.example.timetables.model.StationData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,15 +24,33 @@ import java.util.stream.Collectors;
 @RestController
 public class Controller {
     private DefaultApi defaultApi;
-    private final ObjectMapper jacksonObjectMapper;
+    private TimetablesApi timetablesApi;
+    private ObjectMapper jacksonObjectMapper;
+    private DBService dbService;
+    private FaStaApi faStaApi;
 
 
     public Controller(
             DefaultApi defaultApi,
-            ObjectMapper jacksonObjectMapper) {
+            ObjectMapper jacksonObjectMapper,
+            TimetablesApi timetablesApi,
+            DBService dbService,
+            FaStaApi faStaApi,
+
+            @Value("${api.id}") String apiId,
+            @Value("${api.secret}") String apiSecret
+    ) {
 
         this.defaultApi = defaultApi;
         this.jacksonObjectMapper = jacksonObjectMapper;
+        this.timetablesApi = timetablesApi;
+        this.dbService = dbService;
+        this.faStaApi = faStaApi;
+
+        timetablesApi.getApiClient().addDefaultHeader("DB-Client-Id", apiId);
+        timetablesApi.getApiClient().addDefaultHeader("DB-Api-Key", apiSecret);
+        faStaApi.getApiClient().addDefaultHeader("DB-Client-Id", apiId);
+        faStaApi.getApiClient().addDefaultHeader("DB-Api-Key", apiSecret);
     }
 
     @GetMapping("/db/stationData")
@@ -46,8 +66,9 @@ public class Controller {
 
     @GetMapping("/db/departures")
     @CrossOrigin("http://localhost:4200")
-    public DepartureList getDeparturesById(@RequestParam String id) {
-        Object object = defaultApi.stopsIdDeparturesGet(id, null, null, null, null, null, null, null, null);
+    public DepartureList getDeparturesByStationName(@RequestParam String stationName) {
+        String id = dbService.getEvoNoByStationName(stationName);
+        Object object = defaultApi.stopsIdDeparturesGet(id, null, null, null, 10, null, null, null, null);
         Map<String, Object> map = jacksonObjectMapper.convertValue(object, new TypeReference<Map<String, Object>>() {
         });
         List<Map<String, Object>> departuresMapList = (List<Map<String, Object>>) map.get("departures");
@@ -57,12 +78,48 @@ public class Controller {
         return departureList;
     }
 
-    @GetMapping("/db/autocomplete")
-    @CrossOrigin("http//localhost:4200")
-    public void autocomplete(String pattern){
-        defaultApi.stationsGet(pattern, 3, false, true);
+//    @GetMapping("/db/autocomplete")
+//    @CrossOrigin("http//localhost:4200")
+//    public void autocomplete(String pattern) {
+//        defaultApi.stationsGet(pattern, 3, false, true);
+//    }
 
-
+    @GetMapping("/db/getName")
+    @CrossOrigin("http://localhost:4200")
+    public String getStationNameByEva(@RequestParam String evaNo) {
+        List<StationData> stationData = timetablesApi.stationPatternGet(evaNo).getStation();
+        return dbService.getName(stationData);
     }
 
+    @GetMapping("/db/test")
+    @CrossOrigin("http://localhost:4200")
+    public void getDeparturesByEva(@RequestParam String id) {
+        //String id = dbService.getEvoNoByStationName(stationName);
+        Object object = defaultApi.stopsIdDeparturesGet(
+                id, null, null, null, null, null, null, null, null
+        );
+    }
+
+    @GetMapping("db/elevator")
+    @CrossOrigin("http://localhost:4200")
+    public Boolean hasElevatorByEva(String eva) {
+        Object object = defaultApi.stationsIdGet(eva);
+        Map<String, Object> map = jacksonObjectMapper.convertValue(object, new TypeReference<Map<String, Object>>() {
+        });
+        String id = map.get("nr").toString();
+        List<Facility> facilities;
+        try {
+            facilities = faStaApi.findStationByStationNumber(Long.parseLong(id)).getFacilities();
+        } catch (Exception e) {
+            return false;
+        }
+           assert facilities != null;
+
+        for (Facility facility : facilities) {
+            if (facility.getType() == Facility.TypeEnum.ELEVATOR && facility.getState() == Facility.StateEnum.ACTIVE) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
